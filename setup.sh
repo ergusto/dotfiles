ed7be6dd4e38dff0a49269f58fa1ae82fb424732
Fergus Ruston <fergus.ruston@gmail.com>
2026-04-10 22:14:04 +0100

    setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,10 @@ source utils
 
 os=$(get_os)
 
-echo "Installing for $os"
+echo "Installing apps from Brewfile"
+brew bundle --file=misc/Brewfile
+
+echo "Installing dotfiles for $os"
 
 if [ "$os" == "macos" ]; then
   install_macos
